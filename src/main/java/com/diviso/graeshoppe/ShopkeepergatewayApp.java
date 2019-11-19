ea9557f50c6dea80abe4b3273b833c7b41f20b87
neerajamurali094 <neeraja.m@lxisoft.com>
2019-11-19 16:52:44 +0530

    second commit

--- src/main/java/com/diviso/graeshoppe/ShopkeepergatewayApp.java
+++ src/main/java/com/diviso/graeshoppe/ShopkeepergatewayApp.java
@@ -14,6 +14,8 @@ import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
@@ -22,6 +24,10 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
 
+@ComponentScan( excludeFilters = {
+    @ComponentScan.Filter(com.diviso.graeshoppe.client.ExcludeFromComponentScan.class)
+})
+@EnableFeignClients
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 @EnableDiscoveryClient
